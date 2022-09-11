6ab469b8947f8771a7f3f5bdaa50ca409146e0b1
edison <edisonquu@gmail.com>
2022-09-11 16:32:05 -0400

    Finished removing from an array and learning about for each loops as well as a little bit of => function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
 
+const removeFromArray = function(arrays, ...args) {
+
+    args.forEach(num => {
+        while (arrays.includes(num)) {
+            var indexOf = arrays.indexOf(num)
+            arrays.splice(indexOf, 1)
+        }
+    })
+    return arrays
 };
 
 // Do not edit below this line
