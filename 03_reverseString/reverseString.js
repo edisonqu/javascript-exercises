149a5c1f6fb39a635eca236451bb5dd2e664fc75
edison <edisonquu@gmail.com>
2022-09-11 15:51:20 -0400

    Finished reversing a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    var reversedWord = ""
+    for (let i = words.length-1; i > -1; i--) {
+        reversedWord += words[i]
+    }
+    return reversedWord
 };
 
 // Do not edit below this line
