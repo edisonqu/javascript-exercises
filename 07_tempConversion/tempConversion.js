d55b5077d63b87a3d9991a76f7ec3432614f4641
edison <edisonquu@gmail.com>
2022-09-13 18:39:24 -0400

    celcius and farenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(cel) {
+  return Math.round(((cel-32)*5/9)*10)/10
 };
 
-const ctof = function() {
+const ctof = function(far) {
+  return Math.round(((far*9/5)+32)*10)/10
 
 };
 
